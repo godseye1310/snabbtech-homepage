9faa4c69c2bcf728a8b7a20a4f4205fa567c328c
Avinash <avinashkmg1310@gmail.com>
2025-08-11 18:19:14 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
 	base: "/snabbtech-homepage/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve("index.html"),
+				about: resolve("about.html"),
+				services: resolve("services.html"),
+				// industry: resolve("industry.html"),
+				// portfolio: resolve("portfolio.html"),
+				// partnership: resolve("partnership.html"),
+				// blog: resolve("blog.html"),
+			},
+		},
+	},
 });
